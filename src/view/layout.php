5b70806bdeccffdc6b0e5462a3bfd9704a69a04c
rensampe <rens.ampe@student.howest.be>
2020-01-14 04:54:26 +0100

    header van alle pagina's html/css #1

--- src/view/layout.php
+++ src/view/layout.php
@@ -8,11 +8,46 @@
     <?php echo $css;?>
   </head>
   <body>
-  <ul>
-    <li>dsq</li>
-    <li>dfsq</li>
-  </ul>
+    <header class="header">
+      <nav class="menu">
+        <ul class="menu__wrapper">
+          <div class="menu__items">
+            <li class="menu__item-video"><a href="" class="menu__link-video">video</a></li>
+            <li class="menu__item"><a href="" class="menu__link">tv-gids</a></li>
+            <li class="menu__item"><a href="" class="menu__link">zoekertjes</a></li>
+            <li class="menu__item"><a href="" class="menu__link">abonnement nemen</a></li>
+            <li class="menu__item"><a href="" class="menu__link menu__link-selected">webshop</a></li>
+          </div>
+          <div class="menu__items menu__items-second">
+            <li class="menu__item"><a href="" class="menu__link menu__link-nuinhumo">nu in humo</a></li>
+            <li class="menu__item"><a href="" class="menu__link">login</a></li>
+            <li class="menu__item"><a href="" class="menu__link">registreer</a></li>
+          </div>
+        </ul>
+      </nav>
+      <nav class="menu-two">
+        <ul class="menu-two__items">
+          <div class="menu-two__wrapper">
+            <li class="menu-two__item"><a href="" class="menu-two__link">home</a></li>
+            <li class="menu-two__item"><a href="" class="menu-two__link">actua</a></li>
+            <li class="menu-two__item"><a href="" class="menu-two__link">humor</a></li>
+            <img src="assets/img/humologo.svg" alt="Logo Humo"><h1 class="hidden">Humo</h1>
+            <li class="menu-two__item"><a href="" class="menu-two__link">tv/film</a></li>
+            <li class="menu-two__item"><a href="" class="menu-two__link">muziek</a></li>
+            <li class="menu-two__item"><a href="" class="menu-two__link">boeken</a></li>
+          </div>
+          <li class="menu-two__item-cart">
+            <p class="item-cart">0</p>
+            <a href="" class="menu-two__link">
+              <img src="assets/img/cart.svg" alt="Winkelwagen">
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
     <?php echo $content;?>
+    </main>
     <?php echo $js; ?>
   </body>
 </html>
