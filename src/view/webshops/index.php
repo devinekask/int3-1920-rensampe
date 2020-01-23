2295ea94dcc6a10341ac61885c9248cb5f6cdd36
rensampe <rens.ampe@student.howest.be>
2020-01-23 01:28:57 +0100

    afwerking

--- src/view/webshops/index.php
+++ src/view/webshops/index.php
@@ -119,7 +119,7 @@
         <a class="info" href="index.php?page=detail&amp;id=<?php echo $product['id'];?>">
           <img class="image" src="<?php echo 'assets/img/' . $product['type'] . '/' . $product['type'] . $product['id'] . '.jpg'; ?>" alt="<?php echo $product['title'];?>" width="250" height="300">
           <div class="info__wrapper">
-            <h3 class="subtitle"><?php echo mb_strimwidth($product['title'], 0, 24, "...");?></h3>
+            <h3 class="subtitle"><?php //echo mb_strimwidth($product['title'], 0, 24, "...");?></h3>
             <p class="price"><?php echo money_format('€ %!n', $product['price']); ?></p>
             <p class="type"><?php echo $product['type'];?> - <?php echo $product['subtitle'];?></p>
           </div>
