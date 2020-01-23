3c46b835d49d7f359c10ce211e65d973cb2b89a7
rensampe <rens.ampe@student.howest.be>
2020-01-23 01:43:28 +0100

    heroku foto's laden niet in

--- src/view/webshops/index.php
+++ src/view/webshops/index.php
@@ -117,7 +117,7 @@
     <?php foreach($products as $product): ?>
       <li class="product">
         <a class="info" href="index.php?page=detail&amp;id=<?php echo $product['id'];?>">
-          <img class="image" src="<?php echo '/assets/img/' . $product['type'] . '/' . $product['type'] . $product['id'] . '.jpg'; ?>" alt="<?php echo $product['title'];?>" width="250" height="300">
+          <img class="image" src="<?php echo './assets/img/' . $product['type'] . '/' . $product['type'] . $product['id'] . '.jpg'; ?>" alt="<?php echo $product['title'];?>" width="250" height="300">
           <div class="info__wrapper">
             <h3 class="subtitle"><?php echo $product['title']//mb_strimwidth($product['title'], 0, 24, "...");?></h3>
             <p class="price"><?php echo money_format('€ %!n', $product['price']); ?></p>
